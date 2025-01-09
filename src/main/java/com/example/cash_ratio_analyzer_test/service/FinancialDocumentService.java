70178f2ae88443556774f8d6c39dddb89e410b10
kenichi04 <slow_life_1407@yahoo.co.jp>
2025-01-10 08:25:20 +0900

    FinancialDocument登録処理を仮実装

--- src/main/java/com/example/cash_ratio_analyzer_test/service/FinancialDocumentService.java
+++ src/main/java/com/example/cash_ratio_analyzer_test/service/FinancialDocumentService.java
@@ -20,8 +20,27 @@ public class FinancialDocumentService {
     }
 
     // TODO transactionalアノテーションを付与する
-    public void saveFinancialData(String documentId, List<FinancialData> financialDataList) {
+    // 書類一覧APIレスポンスからdocument作成する場合は、dataが取得できないため、documentIdのみで作成する
+    public void create(String companyId, String documentId) {
+        // TODO 本来はcompanyIdを使って企業情報を取得し、それを使ってFinancialDocumentを作成する
+        // 仮実装, ここでcompanyIdを使用してcompanyの存在チェック
+        if (companyId == null) {
+            throw new RuntimeException("company is not found");
+        }
+        // companyとdocumentは一対多の関係
+        // ここの処理は検討
         var financialDocument = new FinancialDocument(documentId);
+        financialDocumentRepository.save(financialDocument);
+    }
+
+    // TODO transactionalアノテーションを付与する
+    // 書類取得APIレスポンスからの処理を想定。documentは上で作成済にするか、新規作成するかは要検討（dataなしのdocument作成してもよいのか）
+    public void saveFinancialData(String documentId, List<FinancialData> financialDataList) {
+        // TODO documentを取得するか新規作成するかは要検討
+        var financialDocument = financialDocumentRepository.findByDocumentId(documentId);
+        if (financialDocument == null) {
+            financialDocument = new FinancialDocument(documentId);
+        }
         financialDocument.getData().addAll(financialDataList);
         financialDocumentRepository.save(financialDocument);
     }
