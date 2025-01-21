d7011bcbc0f6d002847225a24a4059ed4bc823f5
kenichi04 <slow_life_1407@yahoo.co.jp>
2025-01-22 07:32:40 +0900

    EdinetOutputControllerを非推奨とする
    - @Deprecated付与
    - 開発テスト用のコントローラのため

--- src/main/java/com/example/cash_ratio_analyzer_test/presentation/controller/EdinetOutputController.java
+++ src/main/java/com/example/cash_ratio_analyzer_test/presentation/controller/EdinetOutputController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 // Edinetデータ出力テスト用
+@Deprecated
 @RestController
 @RequestMapping("/edinet/output")
 public class EdinetOutputController {
