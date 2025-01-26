155d7759356e0b303362c12ed93c5a3b11eca2e8
kenichi04 <slow_life_1407@yahoo.co.jp>
2025-01-27 08:36:22 +0900

    書類内容取得処理の修正中
    - タグ情報を持つ表紙ファイルを取得
    - ファイル内容抽出処理の共通化

--- src/main/java/com/example/cash_ratio_analyzer_test/application/service/EdinetFileExtractionService.java
+++ src/main/java/com/example/cash_ratio_analyzer_test/application/service/EdinetFileExtractionService.java
@@ -7,6 +7,8 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -20,6 +22,11 @@ public class EdinetFileExtractionService {
     @Value("${download.targetFilePrefix:}")
     private String targetFilePrefix;
 
+    private static final String INLINE_XBRL_HEADER_FILE_PREFIX = "0000000_header";
+
+    // TODO もう少し具体的なファイル名を指定する
+    private static final String INLINE_XBRL_FIRST_MAIN_FILE_PREFIX = "0101";
+
     /**
      * 指定されたバイト配列（ZIP形式）からターゲットファイルを抽出します。
      *
@@ -27,25 +34,30 @@ public class EdinetFileExtractionService {
      * @return 抽出されたターゲットファイルの内容を表すバイト配列
      */
     public byte[] extractTargetFile(byte[] fetchData) {
+        Map<String, byte[]> targetFileMap = new HashMap<>();
 
-        byte[] fileContent = null;
+        byte[] headerContent = null;
+        byte[] firstMainContent = null;
+        byte[] targetFileContent = null;
         try (
                 var in = new ByteArrayInputStream(fetchData);
                 var zipIn = new ZipInputStream(in)) {
             ZipEntry entry;
 
+            // EDINETからはXBRLとインスタンスXBRLが取得できるが、データ容量等を考慮してインスタンスXBRLを取得する
             while ((entry = zipIn.getNextEntry()) != null) {
+                // 表紙ファイルにタグ情報あり（表紙ファイル無しの場合あり）
+                if (entry.getName().startsWith(INLINE_XBRL_HEADER_FILE_PREFIX)) {
+                    headerContent = extractFileContent(zipIn);
+                }
+                // 表紙ファイル無しの場合、本表の一つ目のファイルにタグ情報あり
+                if (entry.getName().startsWith(INLINE_XBRL_FIRST_MAIN_FILE_PREFIX)) {
+                    firstMainContent = extractFileContent(zipIn);
+                }
                 // 調査：第５【経理の状況】を取得できる想定
                 // TODO 対象ファイルの選定
-                // EDINETからはXBRLとインスタンスXBRLが取得できるが、データ容量等を考慮してインスタンスXBRLを取得する
                 if (entry.getName().startsWith(targetFilePrefix)) {
-                    var out = new ByteArrayOutputStream();
-                    var buffer = new byte[1024];
-                    int len;
-                    while ((len = zipIn.read(buffer)) != -1) {
-                        out.write(buffer, 0, len);
-                    }
-                    fileContent = out.toByteArray();
+                    targetFileContent = extractFileContent(zipIn);
                 }
             }
 
@@ -53,7 +65,24 @@ public class EdinetFileExtractionService {
             e.printStackTrace();
         }
 
-        return fileContent;
+        return targetFileContent;
+    }
+
+    /**
+     * ZIP入力ストリームからファイルの内容を抽出します。
+     *
+     * @param zipIn ZIP入力ストリーム
+     * @return 抽出されたファイルの内容を表すバイト配列
+     * @throws IOException 入出力エラーが発生した場合
+     */
+    private byte[] extractFileContent(ZipInputStream zipIn) throws IOException {
+        var out = new ByteArrayOutputStream();
+        var buffer = new byte[1024];
+        int len;
+        while ((len = zipIn.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
+        return out.toByteArray();
     }
 
     /**
