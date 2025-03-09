46682baeb90f7b88121692520fffcad83899b903
kenichi04 <slow_life_1407@yahoo.co.jp>
2025-03-10 08:21:53 +0900

    EdinetDataFetchServiceTestに異常系のテスト追加

--- src/test/java/com/example/cash_ratio_analyzer_test/application/service/financial/EdinetDataFetchServiceTest.java
+++ src/test/java/com/example/cash_ratio_analyzer_test/application/service/financial/EdinetDataFetchServiceTest.java
@@ -25,11 +25,15 @@ class EdinetDataFetchServiceTest {
 
     private EdinetDataFetchService edinetDataFetchService;
 
+    private static final String TEST_API_URL = "https://example.com/api";
+
+    private static final String TEST_SUBSCRIPTION_KEY = "testKey";
+
     @BeforeEach
     void setUp() {
         apiResponseValidator = spy(new ApiResponseValidator());
         edinetDataFetchService = new EdinetDataFetchService(
-                apiResponseValidator, restTemplate, "https://example.com/api", "testKey");
+                apiResponseValidator, restTemplate, TEST_API_URL, TEST_SUBSCRIPTION_KEY);
     }
 
     @Test
@@ -44,8 +48,8 @@ class EdinetDataFetchServiceTest {
         var mockResponse = new ResponseEntity<>(body, headers, HttpStatus.OK);
 
         when(restTemplate.exchange(
-                eq("https://example.com/api"), eq(HttpMethod.GET), isNull(),
-                eq(byte[].class), eq(documentId), eq(fetchDocumentType.code()), eq("testKey")))
+                eq(TEST_API_URL), eq(HttpMethod.GET), isNull(),
+                eq(byte[].class), eq(documentId), eq(fetchDocumentType.code()), eq(TEST_SUBSCRIPTION_KEY)))
                 .thenReturn(mockResponse);
 
         // when
@@ -59,5 +63,61 @@ class EdinetDataFetchServiceTest {
         verify(apiResponseValidator).validateContentType(mockResponse.getHeaders().getContentType(),
                 MediaType.APPLICATION_OCTET_STREAM, MediaType.APPLICATION_PDF);
         verify(apiResponseValidator).validateResponseBody(mockResponse.getBody());
+    }
+
+    @Test
+    void fetchFinancialData_StatusCodeIsBadRequest() {
+        var documentId = "TEST0001";
+        var fetchDocumentType = FetchDocumentType.XBRL;
+
+        var body = "test-bad-request".getBytes();
+        var mockResponse = new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+
+        when(restTemplate.exchange(
+                eq(TEST_API_URL), eq(HttpMethod.GET), isNull(),
+                eq(byte[].class), eq(documentId), eq(fetchDocumentType.code()), eq(TEST_SUBSCRIPTION_KEY)))
+                .thenReturn(mockResponse);
+
+        assertThrows(RuntimeException.class,
+                () -> edinetDataFetchService.fetchFinancialData(fetchDocumentType, documentId));
+    }
+
+    @Test
+    void fetchFinancialData_ContentTypeIsNotValid() {
+        var documentId = "TEST0001";
+        var fetchDocumentType = FetchDocumentType.XBRL;
+
+        var headers = new HttpHeaders();
+        // コンテンツタイプが不正な値
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        var body = "test-body".getBytes();
+        var mockResponse = new ResponseEntity<>(body, headers, HttpStatus.OK);
+
+        when(restTemplate.exchange(
+                eq(TEST_API_URL), eq(HttpMethod.GET), isNull(),
+                eq(byte[].class), eq(documentId), eq(fetchDocumentType.code()), eq(TEST_SUBSCRIPTION_KEY)))
+                .thenReturn(mockResponse);
+
+        assertThrows(RuntimeException.class,
+                () -> edinetDataFetchService.fetchFinancialData(fetchDocumentType, documentId));
+    }
+
+    @Test
+    void fetchFinancialData_ResponseBodyIsNull() {
+        var documentId = "TEST0001";
+        var fetchDocumentType = FetchDocumentType.XBRL;
+
+        var headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        // レスポンスボディがnull
+        var mockResponse = new ResponseEntity<byte[]>(null, headers, HttpStatus.OK);
+
+        when(restTemplate.exchange(
+                eq(TEST_API_URL), eq(HttpMethod.GET), isNull(),
+                eq(byte[].class), eq(documentId), eq(fetchDocumentType.code()), eq(TEST_SUBSCRIPTION_KEY)))
+                .thenReturn(mockResponse);
+
+        assertThrows(RuntimeException.class,
+                () -> edinetDataFetchService.fetchFinancialData(fetchDocumentType, documentId));
     }
 }
