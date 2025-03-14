bc200959fe6d64c999e44f0902d9657d0eb8d78d
kenichi04 <slow_life_1407@yahoo.co.jp>
2025-03-14 21:08:47 +0900

    テスト修正

--- src/test/java/com/example/cash_ratio_analyzer_test/application/service/financial/xbrl/XbrlHeaderInfoExtractorTest.java
+++ src/test/java/com/example/cash_ratio_analyzer_test/application/service/financial/xbrl/XbrlHeaderInfoExtractorTest.java
@@ -7,6 +7,7 @@ import com.example.cash_ratio_analyzer_test.domain.enums.Currency;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.w3c.dom.Document;
 
@@ -24,24 +25,19 @@ import static org.mockito.Mockito.when;
 class XbrlHeaderInfoExtractorTest {
 
     private XbrlHeaderInfoExtractor xbrlHeaderInfoExtractor;
+
     private XbrlDocumentParser xbrlDocumentParser;
-    private DocumentBuilder documentBuilder;
 
     @BeforeEach
     void setUp() throws ParserConfigurationException {
-        // 依存関係を分離してテストを独立させたいので、XbrlDocumentParserをモック化
-        xbrlDocumentParser = Mockito.mock(XbrlDocumentParser.class);
+        xbrlDocumentParser = new XbrlDocumentParser();
         xbrlHeaderInfoExtractor = new XbrlHeaderInfoExtractor(xbrlDocumentParser);
-        // Mockito.when()を使うときに、documentBuilder.parse()を使うために必要
-        var factory = DocumentBuilderFactory.newInstance();
-        this.documentBuilder = factory.newDocumentBuilder();
     }
 
     @Test
     @DisplayName("有効なヘッダーコンテンツを使用してヘッダー情報を抽出する")
     void extractHeaderInfo_validHeaderContent() throws IOException {
         var headerContent = Files.readAllBytes(Path.of("src/test/resources/xbrl/test_header_ixbrl.htm"));
-        when(xbrlDocumentParser.parseDocumentToDom(headerContent)).thenReturn(parseDocumentToDom(headerContent));
 
         HeaderInfo headerInfo = xbrlHeaderInfoExtractor.extractHeaderInfo(headerContent);
 
@@ -55,13 +51,5 @@ class XbrlHeaderInfoExtractorTest {
         assertEquals("2024-02-29", headerInfo.getDeiInfo().get("jpdei_cor:CurrentFiscalYearEndDateDEI"));
         // 通貨
         assertEquals(Currency.JPY, headerInfo.getCurrency());
-    }
-
-    private Document parseDocumentToDom(byte[] xbrlContent) {
-        try {
-            return documentBuilder.parse(new ByteArrayInputStream(xbrlContent));
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to parse XBRL content: " + e.getMessage());
-        }
     }
 }
